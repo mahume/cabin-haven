e4d0146e9acf9508fe6691bf82d0567ccbe13486
mahume <michael.a.hume@gmail.com>
2019-07-24 13:54:21 -0600

    Adding Banner of Header

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Hero from '../globals/Hero';
 import homeImg from '../../images/lake-dock.jpg';
+import Banner from '../globals/Banner';
 
 const Header = () => {
   return (
     <Hero img={homeImg}>
-      <h1>Hello World</h1>
+      <Banner 
+        greeting='Welcome to'
+        title='cabin'
+        text='Enter description here'
+      />
     </Hero>
   )
 }
