748461e116dfbc3a333247d10ee0ce08afae5179
mahume <michael.a.hume@gmail.com>
2019-07-24 10:47:58 -0600

    Adding Global styles and style.js variables

---
+++ src/styles.js
@@ -0,0 +1,11 @@
+export const setColor = {
+  primaryColor: '#af9a7d',
+  mainWhite: '#fff',
+  mainBlack: '#222',
+  mainGray: '#ececec',
+  lightGray: '#f7f7f7',
+}
+
+export const setFont = {
+  main: "'Lato', sans-serif;",
+}
