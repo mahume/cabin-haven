435b2bc41e0d165c87d744fc9b8da254dbabb8a8
mahume <michael.a.hume@gmail.com>
2019-07-25 09:54:28 -0600

    Adding button, image, & title to About Us section

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import Section from '../globals/Section';
 import Title from '../globals/Title';
+import aboutImg from '../../images/lake-boats.jpg';
+import { setRem, setBorder, setColor, setLetterSpacing, media } from '../../styles';
+import { PrimaryBtn } from '../globals/Button';
 
 const About = () => {
   return (
     <Section>
-      <Title title='About Us' />
+      <AboutCenter>
+        <div className="about-img">
+          <img src={aboutImg} alt="Lake Cabin in Mountains"/>
+        </div>
+        <div className="about-info">
+          <Title title='About Us' />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut, a adipisci. Sed dolorum molestiae sapiente doloremque sint vitae consequatur alias minus cupiditate, quisquam neque quae, id incidunt. Tempore, earum ipsam!</p>
+          <PrimaryBtn>Read More</PrimaryBtn>
+        </div>
+      </AboutCenter>
     </Section>
   )
 }
+
+const AboutCenter = styled.div``
 
 export default About;
